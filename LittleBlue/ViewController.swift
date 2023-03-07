29aba4f4b7ed5a6e6f4c26374d31c694dec8ece2
qq1823988089 <1823988089@qq.com>
2023-03-07 21:56:35 +0800

    Initial Commit

---
+++ LittleBlue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittleBlue
+//
+//  Created by 乔一 on 2023/3/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
